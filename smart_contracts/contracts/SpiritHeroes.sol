d03149d11f9658e0429fee56e7c04f1d06f436fc
Ender13120 <117198798+Ender13120@users.noreply.github.com>
2023-08-20 21:41:52 +0200

    expand nft contract for on-chain utilization

--- smart_contracts/contracts/SpiritHeroes.sol
+++ smart_contracts/contracts/SpiritHeroes.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URISto
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
+//NFTs for EVM-Planewalkers on L1
 contract SpiritHeroes is
     Initializable,
     ERC721Upgradeable,
@@ -14,6 +15,18 @@ contract SpiritHeroes is
     ERC721URIStorageUpgradeable,
     OwnableUpgradeable
 {
+    struct PlanewalkersStats {
+        string name;
+        int health;
+        int ATK;
+        int DEX;
+        int INT;
+        int CONST;
+        int WIS;
+    }
+
+    mapping(uint256 => PlanewalkersStats) private _tokenStats;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -29,10 +42,12 @@ contract SpiritHeroes is
     function safeMint(
         address to,
         uint256 tokenId,
-        string memory uri
+        string memory uri,
+        PlanewalkersStats memory stats
     ) public onlyOwner {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        _tokenStats[tokenId] = stats;
     }
 
     // The following functions are overrides required by Solidity.
@@ -60,7 +75,30 @@ contract SpiritHeroes is
         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        PlanewalkersStats memory stats = _tokenStats[tokenId];
+        string memory json = string(
+            abi.encodePacked(
+                '{"name": "',
+                stats.name,
+                '", "health": ',
+                int2str(stats.health),
+                ', "ATK": ',
+                int2str(stats.ATK),
+                ', "DEX": ',
+                int2str(stats.DEX),
+                ', "INT": ',
+                int2str(stats.INT),
+                ', "CONST": ',
+                int2str(stats.CONST),
+                ', "WIS": ',
+                int2str(stats.WIS),
+                "}"
+            )
+        );
+        return
+            string(
+                abi.encodePacked("data:application/json;charset=utf-8,", json)
+            );
     }
 
     function supportsInterface(
@@ -76,5 +114,29 @@ contract SpiritHeroes is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function int2str(int _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        bool negative = _i < 0;
+        uint i = uint(negative ? -_i : _i);
+        uint j = i;
+        uint length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length + (negative ? 1 : 0));
+        uint k = bstr.length - 1;
+        while (i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (i % 10)));
+            i /= 10;
+        }
+        if (negative) {
+            bstr[0] = "-";
+        }
+        return string(bstr);
     }
 }
